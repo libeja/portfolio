537e9c893fab8885518253936705c4e919be9e92
Jay Ullman <jayullman@gmail.com>
2017-03-15 21:58:16 -0700

    Configure initial project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div>Hey there! I'm on a webpage!</div>
+    )
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader'
+        }),
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new ExtractTextPlugin('styles.css')
+  ]
+
+};
