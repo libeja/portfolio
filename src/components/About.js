f2b335841420caad6a51deee7ce85e0022b00833
Jay Ullman <jayullman@gmail.com>
2017-03-19 23:08:14 -0700

    Add font awesome and styling

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,8 @@ import '../styles/about.scss';
 
 const About = () => {
   return (
-    <section>
+    <section className='about'>
+      <h3>&#60; About Me /&#62;</h3>
       <p>
         Hi, my name is Jay. I develop full-stack web solutions. I
         am looking for a job. Please check out my projects below.
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,17 @@ import '../styles/footer.scss';
 const Footer = () => {
 
   return (
-    <section>
-      Footer section
+    <section className='footer'>
+      <h3>&#60; Find me /&#62;</h3>
+      <div className="icons-container">
+        <a href="https://github.com/libeja" target="_blank"><i className="fa fa-github-square fa-4x" aria-hidden="true"></i></a>
+        <a href="https://www.freecodecamp.com/libeja" target="_blank"><i className="fa fa-free-code-camp fa-4x" aria-hidden="true"></i></a>
+        <a href="#"><i className="fa fa-envelope-o fa-4x" aria-hidden="true"></i></a>
+      </div>
+      <br/>
+      <small>Created by Jay</small>
+      <br/>
+      <small>Using handcrafted HTML, CSS, JavaScript and optimized with React and Webpack</small>
     </section>
   );
 }
