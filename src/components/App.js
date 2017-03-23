01f37d4ff384d1368c8118dc9031ba223ab794c5
Jay Ullman <jayullman@gmail.com>
2017-03-22 19:49:33 -0700

    Update navigation styles

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   
   render() {
     return (
-      <div>
+      <div className='wrapper'>
         <Navigation />
         <Header />
         <About />
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,32 +12,50 @@ class NavBar extends Component {
   constructor() {
     super();
 
+    this.state = {
+      sliderIsOpen: false
+    };
+
     this.toggleSlide = this.toggleSlide.bind(this);
     this.handleScroll = this.handleScroll.bind(this);
+    this.toggleSlideState = this.toggleSlideState.bind(this);
+  }
+
+  // toggles slide state true/false
+  toggleSlideState() {
+    if (this.state.sliderIsOpen) {
+      this.setState({ sliderIsOpen: false });
+    } else {
+      this.setState({ sliderIsOpen: true });
+    }  
   }
 
   // hide sidebar and menu icon when scrolling if sidebar is open
+  // callback from onUnpin from headroom.js
   handleScroll() {
     if (this.sideBar.className.indexOf('hidden') < 0 ) {
       this.sideBar.classList.toggle('hidden');
-      this.hamburgerIcon.classList.toggle('slide');
+      // this.hamburgerIcon.classList.toggle('slide');
+      this.toggleSlideState();
     }
   }
 
   // handles clicking on the hamburger icon
   // toggles whether drop down menu is visible
   toggleSlide() {
-    console.log(this.sideBar);
     this.sideBar.classList.toggle('hidden');
-    this.hamburgerIcon.classList.toggle('slide');
+    // commented out code handled with conditional rendering
+    // this.hamburgerIcon.classList.toggle('slide');
+    
+    this.toggleSlideState();
   }
 
   render() {
     return (
       <Headroom 
         disableInlineStyles 
-        wrapperStyle={{border: '3px solid white'}}
         onUnpin={this.handleScroll}
+        downTolerance={10}
       >
       <nav>
        
@@ -54,8 +72,10 @@ class NavBar extends Component {
         
         {/* wide links */}
         <div className='wide-nav'>
-          <i 
-            className='hamburger-icon fa fa-bars fa-2x'
+          <i
+            className={this.state.sliderIsOpen
+                ? 'hamburger-icon fa fa-times fa-2x slide'
+                : 'hamburger-icon fa fa-bars fa-2x' } 
             onClick={this.toggleSlide}
             ref={(hamburgerIcon) => {this.hamburgerIcon = hamburgerIcon}}            
           ></i>
