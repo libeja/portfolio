6780546218c8bf14eb59cf5a9cda52c0e1329148
Jay Ullman <jayullman@gmail.com>
2017-03-29 20:17:43 -0700

    Add project descriptions

--- src/projectList.js
+++ src/projectList.js
@@ -16,7 +16,7 @@ export const showcaseProjects = [
     techList: ['html', 'css', 'javascript', 'react'],
     gitLink: 'https://github.com/libeja/dungeon-crawler',
     liveLink: 'https://libeja.github.io/dungeon-crawler/',
-    description: 'Rogue-like game adventure inspired game. Collect items and defeat the boss.'
+    description: 'Rogue-like adventure inspired game. Collect items and defeat the boss.'
   },
   {
     name: 'Chore Chart',
@@ -68,83 +68,104 @@ export const moreProjects = [
     techList: ['javascript', 'D3', 'Webpack\u00a02'],
     gitLink: 'https://github.com/libeja/d3-meteorite-landings',
     liveLink: 'https://libeja.github.io/d3-meteorite-landings/',
-    description: ''
+    description: 'displays a visualization of meteorite landings across the globe using the D3.js data ' 
+      + 'visualization library\'s geographic path generator and using geojson and topojson data. '
+      + 'The radius and color of the circles visualizes the mass of the meteorite.'
   },
   {
     name: 'Heatmap with D3.js',
     img: require(assetsDirectory + 'heat-map.png'),
     techList: ['javascript', 'D3', 'Webpack\u00a02'],
     gitLink: 'https://github.com/libeja/d3-heat-map',
-    liveLink: 'https://libeja.github.io/d3-heat-map/'
+    liveLink: 'https://libeja.github.io/d3-heat-map/',
+    description: 'Request JSON data containing the global land-surface temperature from 1753 - 2015.' 
+      + 'Shows variance on a chromatic scale.'
   },
   {
     name: 'Timestamp Microservice',
     img: require(assetsDirectory + 'timestamp.png'),
     techList: ['javascript', 'react', 'fullstack'],
     gitLink: 'https://github.com/libeja/timestamp-microservice',
-    liveLink: 'https://fcc-timestamper-ms.herokuapp.com/'
+    liveLink: 'https://fcc-timestamper-ms.herokuapp.com/',
+    description: 'API endpoint that accepts a string or Unix '
+      + 'timestamp and returns a date in both string and Unix timestamp format as a JSON object.'
   },
   {
     name: 'Request Header Parser Microservice',
     img: require(assetsDirectory + 'req-header.png'),
     techList: ['javascript', 'react', 'fullstack'],
     gitLink: 'https://github.com/libeja/req-header-parser-ms',
-    liveLink: 'https://req-header-ms.herokuapp.com/'
+    liveLink: 'https://req-header-ms.herokuapp.com/',
+    description: 'API endpoint that parses client header information and returns the parsed information as a JSON object.'
   },
   {
     name: 'Scatterplot Graph with D3.js',
     img: require(assetsDirectory + 'scatterplot.png'),
     techList: ['javascript', 'react', 'fullstack', 'D3'],
     gitLink: 'https://github.com/libeja/d3-scatterplot-graph',
-    liveLink: 'https://libeja.github.io/d3-scatterplot-graph/'
+    liveLink: 'https://libeja.github.io/d3-scatterplot-graph/',
+    description: '2 dimensional data visualization of Tour de France doping allegations vs. finishing time.'
   },
   {
     name: 'Force Directed Graph with D3.js',
     img: require(assetsDirectory + 'force-directed-graph.png'),
     techList: ['javascript', 'react', 'fullstack', 'D3'],
     gitLink: 'https://github.com/libeja/d3-force-directed-graph',
-    liveLink: 'https://libeja.github.io/d3-force-directed-graph/'
+    liveLink: 'https://libeja.github.io/d3-force-directed-graph/',
+    description: 'App makes an AJAX request using the axios promise-based library to request ' 
+      + 'National Contiguity information then using D3.js to visualize data as a force-directed graph.'
   },
   {
     name: 'Recipe Box',
     img: require(assetsDirectory + 'recipe-box.png'),
     techList: ['javascript', 'react', 'redux'],
     gitLink: 'https://github.com/libeja/recipe-box',
-    liveLink: 'https://libeja.github.io/recipe-box/'
+    liveLink: 'https://libeja.github.io/recipe-box/',
+    description: 'An app created with React.js and Redux that uses localStorage to store state. ' 
+      + 'A user can enter, edit, or delete recipes.'
   },
   {
     name: 'Markdown Previewer',
     img: require(assetsDirectory + 'markdown-previewer.png'),
     techList: ['javascript', 'react'],
     gitLink: 'https://github.com/libeja/markdown-previewer',
-    liveLink: 'https://libeja.github.io/markdown-previewer/'
+    liveLink: 'https://libeja.github.io/markdown-previewer/',
+    description: 'Type text with Markdown syntax in one pane, and see it converted in real-time to ' 
+      + 'HTML in a second pane. Build with React.'
   },
   {
     name: 'Bar Chart with D3.js',
     img: require(assetsDirectory + 'bar-chart.png'),
     techList: ['javascript', 'react', 'D3'],
     gitLink: 'https://github.com/libeja/d3-bar-chart',
-    liveLink: 'https://libeja.github.io/d3-bar-chart/'
+    liveLink: 'https://libeja.github.io/d3-bar-chart/',
+    description: 'App makes an AJAX request for JSON data from the Federal Reserve Economic Data and ' 
+      + 'then visualizes that data using D3.js.'
   },
   {
     name: 'Random Quote Machine',
     img: require(assetsDirectory + 'random-quote-machine.png'),
     techList: ['javascript', 'html', 'css'],
     gitLink: 'http://codepen.io/jugglnaut/pen/rrOPxY/',
-    liveLink: 'http://codepen.io/jugglnaut/full/rrOPxY/'
+    liveLink: 'http://codepen.io/jugglnaut/full/rrOPxY/',
+    description: 'App generates a random quote from received AJAX request from the forismatic API. ' 
+      + 'User can request more, and tweet the quotes.'
   },
   {
     name: 'Wikipedia Viewer',
     img: require(assetsDirectory + 'wikipedia-viewer.png'),
     techList: ['javascript', 'html', 'css'],
     gitLink: 'http://codepen.io/jugglnaut/pen/GjWVdg',
-    liveLink: 'http://codepen.io/jugglnaut/full/GjWVdg/'
+    liveLink: 'http://codepen.io/jugglnaut/full/GjWVdg/',
+    description: 'Utilized Wikipedia’s API to search for Wikipedia articles and display within the interface.'
   },
   {
     name: 'Local Weather',
     img: require(assetsDirectory + 'local-weather.png'),
     techList: ['javascript', 'html', 'css'],
     gitLink: 'http://codepen.io/jugglnaut/pen/QKEPRR',
-    liveLink: 'http://codepen.io/jugglnaut/full/QKEPRR/'
+    liveLink: 'http://codepen.io/jugglnaut/full/QKEPRR/',
+    description: 'Requests weather data from Darksky’s API and gives the user the current weather ' 
+      + 'conditions based on client location. Visualized conditions with appropriate weather icon.'
   },
 ];
