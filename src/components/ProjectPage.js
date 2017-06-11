2dd2c5d929eb9947a9446307e23632c9610170f9
Jay Ullman <jayullman@gmail.com>
2017-06-10 22:21:48 -0700

    Add ScrollToTopOnMount component in ProjectPage

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { showcaseProjects, moreProjects } from '../projectList';
+
+import '../styles/projectPage.scss';
+
+const LinkToPortfolio = () => (
+  <Link className='portfolio-link' to='/#projects'>
+    <i className="fa fa-angle-left" aria-hidden="true"></i>
+    <i className="fa fa-angle-left" aria-hidden="true"></i>
+    {' '}Back to portfolio
+  </Link>
+);
+
+const ExternalLink = (props) => (
+  <div className='external-link'>
+    <a href={props.url} target='_blank'>
+      {props.children}{' '} 
+      <i className="fa fa-angle-right" aria-hidden="true"></i>
+      <i className="fa fa-angle-right" aria-hidden="true"></i>
+    </a>
+  </div>
+);
+
+
+/**
+ * Use scroll restoration to reset window to top
+ * from react router docs: https://reacttraining.com/react-router/web/guides/scroll-restoration
+ */
+class ScrollToTopOnMount extends Component {
+  componentDidMount(prevProps) {
+    window.scrollTo(0, 0)
+  }
+
+  render() {
+    return null
+  }
+}
 
 const ProjectPage = (props) => {
   const projectUrl = props.match.params.projectUrl;
@@ -11,17 +47,47 @@ const ProjectPage = (props) => {
   });
   if (!project) {
     project = moreProjects.find((project) => {
-      return projectUrl === projectUrl;
+      return projectUrl === project.projectUrl;
     });
   }
 
-  console.log(project);
-
   return (
-    <div>
+    <div className='project-page'>
+
+      {/* scroll window to top */}
+      <ScrollToTopOnMount />
+
+      <LinkToPortfolio />
       <h1>{project.name}</h1>
-      <h2>{project.description}</h2>
-      <img className='projectImage-large' src={project.img} />
+      <h4>{project.description}</h4>
+      <ExternalLink url={project.liveLink}>
+        Check out the live site
+      </ExternalLink>
+      <img className='project-image-large' src={project.img} />
+
+    
+      <ExternalLink url={project.gitLink}>
+        Check out the project source code
+      </ExternalLink>
+      {project.userStories && project.userStories.length > 0 &&
+      <div className='project-user-stories-section'>
+        <h3>User Stories</h3>
+        <div className='user-stories-container'>
+          <ol className='user-stories'>
+            {project.userStories.map((item, index) =>
+              <li key={item}>{item}</li>
+            )}
+          </ol>
+        </div>
+      </div>}
+      {project.longDescription && project.longDescription.length > 0 &&
+        <div className='project-description-section'>
+          <h3>Project Description</h3>
+          {project.longDescription.map(p => <p key={p}>{p}</p>)}
+        </div>}
+
+      
+      <LinkToPortfolio />
     </div>
   );
 }
