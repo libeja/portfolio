a5d4977b7561f218f36d5a781d42c62faf3fa47d
Jay Ullman <jayullman@gmail.com>
2017-07-29 23:31:01 -0700

    Update header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -61,7 +61,7 @@ class Header extends Component {
         <ScrollableAnchor id="home">
           <div className="name-container animate">
             <h1>Jay Ullman</h1>
-            <h2 className="sub-title">JavaScript Engineer</h2>
+            <h2>JavaScript Engineer</h2>
           </div>
         </ScrollableAnchor>
       </section>
