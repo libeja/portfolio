c084106365af8902048ce251bbead947b56cf600
Jay Ullman <jayullman@gmail.com>
2017-07-31 18:16:28 -0700

    Fix width issue for resume on mobile

--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import ResumeDownload from './ResumeDownload';
 
 const ResumePage = () =>
   <div className="resume-page">
     <iframe
       className="resume-frame"
       src="https://drive.google.com/file/d/0B_nswDKKuanzMUpjdTVrZEVCVGc/preview"
-      width="100%"
+      width="99%"
       height="600"
     />
+    <ResumeDownload />
   </div>;
 
 export default ResumePage;
