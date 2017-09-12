8943418ac79570b4b7f71a880ea9d585f56e4a90
Jay Ullman <jayullman@gmail.com>
2017-09-11 20:30:52 -0700

    Update GitHub urls

--- src/projectList.js
+++ src/projectList.js
@@ -7,7 +7,7 @@ export const showcaseProjects = [
     projectUrl: 'pinbored',
     img: require(assetsDirectory + 'pinbored.png'),
     techList: ['html', 'css', 'javascript', 'react', 'react router', 'mongoDB', 'node', 'express'],
-    gitLink: 'https://github.com/libeja/pinbored',
+    gitLink: 'https://github.com/jayullman/pinbored',
     liveLink: 'https://pinbored.herokuapp.com/',
     description:
       "Final full stack application for freeCodeCamp's back end certification. Pin and like images" +
@@ -30,7 +30,7 @@ export const showcaseProjects = [
     projectUrl: 'voting-booth',
     img: require(assetsDirectory + 'voting-booth.png'),
     techList: ['html', 'css', 'javascript', 'react', 'react router', 'mongoDB', 'node', 'express'],
-    gitLink: 'https://github.com/libeja/voting-app',
+    gitLink: 'https://github.com/jayullman/voting-app',
     liveLink: 'https://fcc-voting-booth.herokuapp.com/',
     description:
       'Full stack application that allows authenticated users to create custom polls for any visitor to vote on.',
@@ -53,7 +53,7 @@ export const showcaseProjects = [
     projectUrl: 'book-club',
     img: require(assetsDirectory + 'bookclub.png'),
     techList: ['html', 'css', 'javascript', 'react', 'react router', 'mongoDB', 'node', 'express'],
-    gitLink: 'https://github.com/libeja/book-trading-club',
+    gitLink: 'https://github.com/jayullman/book-trading-club',
     liveLink: 'https://the-book-club.herokuapp.com/',
     description:
       "Full stack application allows users to search for books using the Google Books API and add to user's collection, stored in MongoDB.",
@@ -74,8 +74,8 @@ export const showcaseProjects = [
     projectUrl: 'chorechart',
     img: require(assetsDirectory + 'chore-chart.png'),
     techList: ['html', 'css', 'javascript', 'react', 'redux'],
-    gitLink: 'https://github.com/libeja/chore-chart',
-    liveLink: 'https://libeja.github.io/chore-chart/',
+    gitLink: 'https://github.com/jayullman/chore-chart',
+    liveLink: 'https://jayullman.github.io/chore-chart/',
     description: 'Final Project for CS50. A chore tracker for housemates utilizing React and Redux.',
     longDescription: [
       "<a target='_blank' href='https://www.youtube.com/watch?v=bEA-askLQUU&feature=youtu.be'>View my 2 minute presentation</a>",
@@ -95,8 +95,8 @@ export const showcaseProjects = [
     projectUrl: 'dungeon-crawler',
     img: require(assetsDirectory + 'dungeon-crawler.png'),
     techList: ['html', 'css', 'javascript', 'react'],
-    gitLink: 'https://github.com/libeja/dungeon-crawler',
-    liveLink: 'https://libeja.github.io/dungeon-crawler/',
+    gitLink: 'https://github.com/jayullman/dungeon-crawler',
+    liveLink: 'https://jayullman.github.io/dungeon-crawler/',
     description: 'Rogue-like adventure inspired game. Collect items and defeat the boss.',
     longDescription: [
       'A random dungeon is created and the player must collect weapons and armor to become stronger in order to beat the enemies. The player gets stronger when leveling up. The boss is hidden in one room that is locked with a key the player must find somewhere in the dungeon.',
@@ -122,7 +122,7 @@ export const showcaseProjects = [
     projectUrl: 'nightlife',
     img: require(assetsDirectory + 'nightlife.png'),
     techList: ['html', 'css', 'javascript', 'react', 'mongoDB', 'node', 'express'],
-    gitLink: 'https://github.com/libeja/nightlife-coordination',
+    gitLink: 'https://github.com/jayullman/nightlife-coordination',
     liveLink: 'https://fcc-nightlife-app-jay.herokuapp.com/',
     description: 'Select locations you will be attending. See how many users are going to different locations.',
     longDescription: [],
@@ -138,8 +138,8 @@ export const showcaseProjects = [
     projectUrl: 'game-of-life',
     img: require(assetsDirectory + 'game-of-life.png'),
     techList: ['html', 'css', 'javascript', 'react'],
-    gitLink: 'https://github.com/libeja/game-of-life',
-    liveLink: 'https://libeja.github.io/game-of-life/',
+    gitLink: 'https://github.com/jayullman/game-of-life',
+    liveLink: 'https://jayullman.github.io/game-of-life/',
     description: "Simple life simulation based on John Conway's Game of Life.",
     longDescription: [
       "Here's an explanation of Conway's Game of Life from John Conway himself: <a target='_blank' href='https://www.youtube.com/watch?v=E8kUJL04ELA'>youtube video</a>",
@@ -160,8 +160,8 @@ export const showcaseProjects = [
     projectUrl: 'simon',
     img: require(assetsDirectory + 'simon.png'),
     techList: ['html', 'css', 'javascript'],
-    gitLink: 'https://github.com/libeja/simon',
-    liveLink: 'https://libeja.github.io/simon/',
+    gitLink: 'https://github.com/jayullman/simon',
+    liveLink: 'https://jayullman.github.io/simon/',
     description: 'Recreation of the elecronic Simon Game.',
     longDescription: [
       "This project was the final advanced front end project for the freeCodeCamp front end certificaiton. It is a recreation of the classic electronic simon game from Milton Bradly. It fully mimics the original game's functionality and can be played in two different modes: normal and strict. In strict mode, any mistake will reset the game back to the first round.",
@@ -202,7 +202,7 @@ export const showcaseProjects = [
     projectUrl: 'stocks',
     img: require(assetsDirectory + 'stocks.png'),
     techList: ['html', 'css', 'javascript', 'react', 'mongoDB', 'node', 'express', 'socket.io'],
-    gitLink: 'https://github.com/libeja/stocks',
+    gitLink: 'https://github.com/jayullman/stocks',
     liveLink: 'https://fcc-chart-the-stockmarket.herokuapp.com/',
     description: 'Full stack multi-user application updated in real-time using Websockets and socket.io.',
     longDescription: [
@@ -226,8 +226,8 @@ export const moreProjects = [
     projectUrl: 'recipe-box',
     img: require(assetsDirectory + 'recipe-box.png'),
     techList: ['javascript', 'react', 'redux'],
-    gitLink: 'https://github.com/libeja/recipe-box',
-    liveLink: 'https://libeja.github.io/recipe-box/',
+    gitLink: 'https://github.com/jayullman/recipe-box',
+    liveLink: 'https://jayullman.github.io/recipe-box/',
     description:
       'An app created with React.js and Redux that uses localStorage to store state. ' +
       'A user can enter, edit, or delete recipes.',
@@ -246,7 +246,7 @@ export const moreProjects = [
     projectUrl: 'file-metadata-ms',
     img: require(assetsDirectory + 'file-metadata.png'),
     techList: ['javascript', 'react', 'node', 'express'],
-    gitLink: 'https://github.com/libeja/file-metadata-ms',
+    gitLink: 'https://github.com/jayullman/file-metadata-ms',
     liveLink: 'https://file-sizer-fcc.herokuapp.com/',
     description: 'Node/Express app that accepts a file and returns the size of the file in JSON format',
     longDescription: [],
@@ -260,7 +260,7 @@ export const moreProjects = [
     projectUrl: 'image-search-ms',
     img: require(assetsDirectory + 'image-search.png'),
     techList: ['javascript', 'react', 'node', 'express', 'mongoDB'],
-    gitLink: 'https://github.com/libeja/image-search',
+    gitLink: 'https://github.com/jayullman/image-search',
     liveLink: 'https://fccapi-imagesearch.herokuapp.com/',
     description:
       'This API microservice will shorten a valid URL. The shortened URLs are stored in a MongoDB database. ' +
@@ -277,8 +277,8 @@ export const moreProjects = [
     projectUrl: 'markdown-previewer',
     img: require(assetsDirectory + 'markdown-previewer.png'),
     techList: ['javascript', 'react'],
-    gitLink: 'https://github.com/libeja/markdown-previewer',
-    liveLink: 'https://libeja.github.io/markdown-previewer/',
+    gitLink: 'https://github.com/jayullman/markdown-previewer',
+    liveLink: 'https://jayullman.github.io/markdown-previewer/',
     description:
       'Type text with Markdown syntax in one pane, and see it converted in real-time to ' +
       'HTML in a second pane. Built with React.',
@@ -293,7 +293,7 @@ export const moreProjects = [
     projectUrl: 'javascript-calc',
     img: require(assetsDirectory + 'js-calculator.png'),
     techList: ['html', 'css', 'javascript', 'react'],
-    gitLink: 'https://github.com/libeja/JavaScript-Calculator',
+    gitLink: 'https://github.com/jayullman/JavaScript-Calculator',
     liveLink: 'http://codepen.io/jugglnaut/full/zoqpKp/',
     description: 'Functioning calculator built with JavaScript',
     longDescription: [],
@@ -323,8 +323,8 @@ export const moreProjects = [
     projectUrl: 'd3-meteorite',
     img: require(assetsDirectory + 'meteorite-landings.png'),
     techList: ['javascript', 'D3', 'Webpack\u00a02'],
-    gitLink: 'https://github.com/libeja/d3-meteorite-landings',
-    liveLink: 'https://libeja.github.io/d3-meteorite-landings/',
+    gitLink: 'https://github.com/jayullman/d3-meteorite-landings',
+    liveLink: 'https://jayullman.github.io/d3-meteorite-landings/',
     description: 'D3 visualization of meteorite landings across the globe using geojson and topojson data.',
     longDescription: [
       "This project displays a visualization of meteorite landings across the globe using the <a href='https://d3js.org/' target='_blank' D3.js data visualization library's geographic path generator and using geojson and topojson data. The radius and color of the circles visualizes the mass of the meteorite.",
@@ -341,8 +341,8 @@ export const moreProjects = [
     projectUrl: 'd3-heatmap',
     img: require(assetsDirectory + 'heat-map.png'),
     techList: ['javascript', 'D3', 'Webpack\u00a02'],
-    gitLink: 'https://github.com/libeja/d3-heat-map',
-    liveLink: 'https://libeja.github.io/d3-heat-map/',
+    gitLink: 'https://github.com/jayullman/d3-heat-map',
+    liveLink: 'https://jayullman.github.io/d3-heat-map/',
     description:
       'Request JSON data containing the global land-surface temperature from 1753 - 2015.' +
       'Shows variance on a chromatic scale.',
@@ -358,7 +358,7 @@ export const moreProjects = [
     projectUrl: 'url-shortening-ms',
     img: require(assetsDirectory + 'url-shortener.png'),
     techList: ['javascript', 'react', 'node', 'express', 'mongoDB'],
-    gitLink: 'https://github.com/libeja/url-shortener',
+    gitLink: 'https://github.com/jayullman/url-shortener',
     liveLink: 'https://urlshorten-fcc.herokuapp.com/',
     description: 'This API microservice will shorten a valid URL. The shortened URLs are stored in a MongoDB database.',
     longDescription: [],
@@ -373,7 +373,7 @@ export const moreProjects = [
     projectUrl: 'timestamp-ms',
     img: require(assetsDirectory + 'timestamp.png'),
     techList: ['javascript', 'react', 'fullstack'],
-    gitLink: 'https://github.com/libeja/timestamp-microservice',
+    gitLink: 'https://github.com/jayullman/timestamp-microservice',
     liveLink: 'https://fcc-timestamper-ms.herokuapp.com/',
     description:
       'API endpoint that accepts a string or Unix ' +
@@ -390,7 +390,7 @@ export const moreProjects = [
     projectUrl: 'request-header-parser-ms',
     img: require(assetsDirectory + 'req-header.png'),
     techList: ['javascript', 'react', 'fullstack'],
-    gitLink: 'https://github.com/libeja/req-header-parser-ms',
+    gitLink: 'https://github.com/jayullman/req-header-parser-ms',
     liveLink: 'https://req-header-ms.herokuapp.com/',
     description:
       'API endpoint that parses client header information and returns the parsed information as a JSON object.',
@@ -402,8 +402,8 @@ export const moreProjects = [
     projectUrl: 'd3-scatterplot',
     img: require(assetsDirectory + 'scatterplot.png'),
     techList: ['javascript', 'react', 'fullstack', 'D3'],
-    gitLink: 'https://github.com/libeja/d3-scatterplot-graph',
-    liveLink: 'https://libeja.github.io/d3-scatterplot-graph/',
+    gitLink: 'https://github.com/jayullman/d3-scatterplot-graph',
+    liveLink: 'https://jayullman.github.io/d3-scatterplot-graph/',
     description: '2 dimensional data visualization of Tour de France doping allegations vs. finishing time.',
     longDescription: [],
     userStories: [
@@ -416,8 +416,8 @@ export const moreProjects = [
     projectUrl: 'd3-force-directed-graph',
     img: require(assetsDirectory + 'force-directed-graph.png'),
     techList: ['javascript', 'react', 'fullstack', 'D3'],
-    gitLink: 'https://github.com/libeja/d3-force-directed-graph',
-    liveLink: 'https://libeja.github.io/d3-force-directed-graph/',
+    gitLink: 'https://github.com/jayullman/d3-force-directed-graph',
+    liveLink: 'https://jayullman.github.io/d3-force-directed-graph/',
     description: 'App makes an AJAX request and visualizes JSON as a force-directed graph to show national contiguity',
     longDescription: [],
     userStories: [
@@ -430,8 +430,8 @@ export const moreProjects = [
     projectUrl: 'd3-barchart',
     img: require(assetsDirectory + 'bar-chart.png'),
     techList: ['javascript', 'react', 'D3'],
-    gitLink: 'https://github.com/libeja/d3-bar-chart',
-    liveLink: 'https://libeja.github.io/d3-bar-chart/',
+    gitLink: 'https://github.com/jayullman/d3-bar-chart',
+    liveLink: 'https://jayullman.github.io/d3-bar-chart/',
     description:
       'App makes an AJAX request for JSON data from the Federal Reserve Economic Data and ' +
       'then visualizes that data using D3.js.',
@@ -489,8 +489,8 @@ export const moreProjects = [
     projectUrl: 'local-weather',
     img: require(assetsDirectory + 'local-weather.png'),
     techList: ['javascript', 'html', 'css'],
-    gitLink: 'https://github.com/libeja/local-weather',
-    liveLink: 'https://libeja.github.io/local-weather/index.html',
+    gitLink: 'https://github.com/jayullman/local-weather',
+    liveLink: 'https://jayullman.github.io/local-weather/index.html',
     description:
       "Requests weather data from OpenWeatherMap's API and displays the local weather based on client location.",
     longDescription: [],
